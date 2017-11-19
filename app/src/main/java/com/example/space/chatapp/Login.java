1fd1608bf1f36537bef9433b1067fca43f819d49
sabra <mohamed2006fs@gmail.com>
2017-11-20 00:35:04 +0200

    Implement the Login activity with simple UI and tested with firebase waiting for the rest activity to link the program flow.

---
+++ app/src/main/java/com/example/space/chatapp/Login.java
@@ -0,0 +1,89 @@
+package com.example.space.chatapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+
+    private Button loginButton, registerButton;
+    private EditText email, password;
+    private String emailString, passwordString;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.loginButton);
+        registerButton = findViewById(R.id.register);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        progressBar = findViewById(R.id.progressBar);
+
+        // Initialize FirebaseAuth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+//        registerButton.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                startActivity(new Intent(Login.this, Register.class));
+//                finish();
+//            }
+//        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                emailString = email.getText().toString();
+                passwordString = password.getText().toString();
+
+                if (TextUtils.isEmpty(emailString)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(passwordString)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                // authenticate user
+                mFirebaseAuth.signInWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressBar.setVisibility(View.GONE);
+                        if (!task.isSuccessful()) {
+                            // here is error
+                            if (passwordString.length() < 6) {
+                                password.setError(getString(R.string.minimum_password));
+                            } else {
+                                Toast.makeText(Login.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Toast.makeText(Login.this, "Welcome Back :D", Toast.LENGTH_LONG).show();
+//                          startActivity(new Intent(Login.this, Users.class));
+//                          finish();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
