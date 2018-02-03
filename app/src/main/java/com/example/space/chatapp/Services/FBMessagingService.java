a6b91b888d7f050e2578222764d86efb9b2ba0a9
GehadFathy <gehadfathy99@gmail.com>
2018-02-03 21:54:41 +0200

    pushNotifications+notificationFregment but fails in refreshing data and click on notification when app is terminated

---
+++ app/src/main/java/com/example/space/chatapp/Services/FBMessagingService.java
@@ -0,0 +1,6 @@
+/**
+ * Created by gehad on 02/02/18.
+ */
+
+public class FBMessagingService {
+}
---
+++ app/src/main/java/com/example/space/chatapp/models/Request.java
@@ -0,0 +1,8 @@
+package com.example.space.chatapp.models;
+
+/**
+ * Created by gehad on 03/02/18.
+ */
+
+public class Request {
+}
